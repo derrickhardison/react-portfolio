59a8f49998578b6d46e37558e800c8bef0f37ff6
dlhardi <derricklhardison@gmail.com>
2021-02-23 01:19:33 -0500

    Updated file structure to include needed components and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Header/Header";
+import Project from "./Components/Project/Project";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Project />
     </div>
   );
 }
